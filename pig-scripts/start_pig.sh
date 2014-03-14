a6d35df8f34f0b5f0d42f65f1cdf07867f5396e5
Shanmugam <shanmuga.s@goorulearning.org>
2014-03-14 19:37:36 +0530

    Removed unwanted files

--- pig-scripts/start_pig.sh
+++
@@ -1,3 +0,0 @@
-datestamp=`date '+%Y-%m-%d' --date="1 days ago"`
-/usr/local/pig/bin/pig_cassandra -x local  -param DATE=$datestamp /home/hduser/event-api/pig-scripts/world_aggregation_from_hourly_to_all.pig
-
