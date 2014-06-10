aaf7697d234180796fd0d9ea9f9ae49646315fa1
daniel <arokiadaniel.a@goorulearning.org>
2014-06-10 12:59:23 +0530

    Added extra mutation batch

--- core/src/main/java/org/logger/event/cassandra/loader/dao/LiveDashBoardDAOImpl.java
+++ core/src/main/java/org/logger/event/cassandra/loader/dao/LiveDashBoardDAOImpl.java
@@ -88,8 +88,8 @@ public class LiveDashBoardDAOImpl  extends BaseDAOCassandraImpl implements LiveD
 			for(String key : keys) {
 				generateCounter(key,eventName,1, m);
 				generateCounter(key+SEPERATOR+gooruUId,eventName,1, m);
-				generateAggregator(key, LASTACCESSED, createdOn, m);
-				generateAggregator(key, LASTACCESSEDUSER, gooruUId, m);
+				generateAggregator(key, eventName+SEPERATOR+LASTACCESSED, createdOn, m);
+				generateAggregator(key, eventName+SEPERATOR+LASTACCESSEDUSER, gooruUId, m);
 			}
 			try {
 	            m.execute();
