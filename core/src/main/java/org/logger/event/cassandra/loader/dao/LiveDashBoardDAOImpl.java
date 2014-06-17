137a22099b990fb044fee435e811c61ba0bd0d3f
daniel <arokiadaniel.a@goorulearning.org>
2014-06-17 19:56:17 +0530

    Removed logger info

--- core/src/main/java/org/logger/event/cassandra/loader/dao/LiveDashBoardDAOImpl.java
+++ core/src/main/java/org/logger/event/cassandra/loader/dao/LiveDashBoardDAOImpl.java
@@ -96,7 +96,6 @@ public class LiveDashBoardDAOImpl  extends BaseDAOCassandraImpl implements LiveD
 			List<String> keys = this.generateYMWDKey(eventMap.get(STARTTIME));
 			MutationBatch m = getKeyspace().prepareMutationBatch().setConsistencyLevel(DEFAULT_CONSISTENCY_LEVEL);
 			for(String key : keys) {
-				logger.info("Keysss : {}");
 				generateCounter(key,eventName,1, m);
 				generateCounter(key+SEPERATOR+gooruUId,eventName,1, m);
 				generateCounter(eventName,key,1, m);
@@ -157,10 +156,8 @@ public class LiveDashBoardDAOImpl  extends BaseDAOCassandraImpl implements LiveD
 	
 	public List<String> generateYMWDKey(String eventTime){
 		List<String> returnDate = new ArrayList<String>();	
-		logger.info("dashboardKeys : {}",dashboardKeys);
 		if(dashboardKeys != null){
 			for(String key : dashboardKeys.split(",")){
-				logger.info("key : {}",key);
 				customDateFormatter = new SimpleDateFormat(key);
 				Date eventDateTime = new Date(Long.valueOf(eventTime));
 				String rowKey = customDateFormatter.format(eventDateTime).toString();
