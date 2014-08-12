b164af425e13e5c605262900db9336d95e4831df
daniel <arokiadaniel.a@goorulearning.org>
2014-08-12 19:38:22 +0530

    Debug

--- core/src/main/java/org/logger/event/cassandra/loader/EsMappingUtil.java
+++ core/src/main/java/org/logger/event/cassandra/loader/EsMappingUtil.java
@@ -1,10 +1,13 @@
 package org.logger.event.cassandra.loader;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.core.io.ClassPathResource;
@@ -23,13 +26,16 @@ public class EsMappingUtil {
 	public static String getConfig(String indexType,
 			String configFile) {
 		String content = null;
-		String settingsPath = "resources/config/index/" + indexType + "/_" + configFile + ".json";
+		String settingsPath = "/config/index/" + indexType + "/_" + configFile + ".json";
+		
+		Path currentRelativePath = Paths.get("");
+		String s = currentRelativePath.toAbsolutePath().toString();
+		String filePath = s+settingsPath;
+		System.out.print("Current Location : " + s);
+		File file = new File(filePath);		
 		try {
-			Resource resource = new ClassPathResource(settingsPath);
-			System.out.print("File Location : " + resource.getURI());
-			System.out.print("File Locations : " + resource.getFile());
-			content = FileUtils.readFileToString(resource.getFile());
-			
+			System.out.print("File Locations : " + file.getAbsolutePath());
+			content = FileUtils.readFileToString(file);
 		} catch (FileNotFoundException e) {
 			InputStream resourceStream = EsMappingUtil.class.getClassLoader().getResourceAsStream(settingsPath);
 			content = readFileAsString(resourceStream);
