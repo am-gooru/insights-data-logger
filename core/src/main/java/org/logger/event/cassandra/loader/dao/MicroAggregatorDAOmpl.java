daf072fc84178e13760e179120a6d6255ef84a53
daniel <arokiadaniel.a@goorulearning.org>
2014-09-20 11:14:06 +0530

    Debugging

--- core/src/main/java/org/logger/event/cassandra/loader/dao/MicroAggregatorDAOmpl.java
+++ core/src/main/java/org/logger/event/cassandra/loader/dao/MicroAggregatorDAOmpl.java
@@ -804,14 +804,19 @@ public class MicroAggregatorDAOmpl extends BaseDAOCassandraImpl implements Micro
 		List<String> pathwayIds = new ArrayList<String>();
 		for(String pathwayId : parentIds){
 			String type = null;
-			Rows<String, String>  resourcesDetail = baseCassandraDao.readIndexedColumn(ColumnFamily.DIMRESOURCE.getColumnFamily(),"gooru_oid",pathwayId);
-
-			if(resourcesDetail != null){
+			//Rows<String, String>  resourcesDetail = baseCassandraDao.readIndexedColumn(ColumnFamily.DIMRESOURCE.getColumnFamily(),"gooru_oid",pathwayId);
+			
+			ColumnList<String> resourcesDetail = baseCassandraDao.readWithKey(ColumnFamily.DIMRESOURCE.getColumnFamily(), "GLP~" + pathwayId);
+			
+			type = resourcesDetail.getStringValue("type_name", null);
+			
+			/*if(resourcesDetail != null){
 				for(Row<String, String> resource : resourcesDetail){
 					type =  resource.getColumns().getColumnByName("type_name") == null ? null : resource.getColumns().getColumnByName("type_name").getStringValue() ;
 					logger.info("resource type : " + type);
 				 }
 			}
+			*/
 			
 			logger.info("pathwayId : " + pathwayId); 
 			
