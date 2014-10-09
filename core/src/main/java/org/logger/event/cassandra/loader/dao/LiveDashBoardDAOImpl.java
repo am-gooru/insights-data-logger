008cac93d594417f788aea1af86c9ffb0b9f8573
daniel <arokiadaniel.a@goorulearning.org>
2014-10-09 19:52:42 +0530

    view count expire column

--- core/src/main/java/org/logger/event/cassandra/loader/dao/LiveDashBoardDAOImpl.java
+++ core/src/main/java/org/logger/event/cassandra/loader/dao/LiveDashBoardDAOImpl.java
@@ -350,7 +350,7 @@ public class LiveDashBoardDAOImpl  extends BaseDAOCassandraImpl implements LiveD
 		
 		logger.info("Key- view : {} ",VIEWS+SEPERATOR+dateKey);
 		
-		baseDao.generateNonCounter(ColumnFamily.MICROAGGREGATION.getColumnFamily(),VIEWS+SEPERATOR+dateKey, secondDateFormatter.format(new Date()).toString()+SEPERATOR+eventMap.get(CONTENTGOORUOID), eventMap.get(CONTENTGOORUOID), m);
+		baseDao.generateTTLColumns(ColumnFamily.MICROAGGREGATION.getColumnFamily(),VIEWS+SEPERATOR+dateKey,eventMap.get(CONTENTGOORUOID), eventMap.get(CONTENTGOORUOID), 2592000,m);
 		
 		try {
             m.execute();
