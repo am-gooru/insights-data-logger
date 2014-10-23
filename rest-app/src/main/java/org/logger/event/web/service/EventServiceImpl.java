6e5ebaee325ecbd24fa026f3839d719960b8d8b1
daniel <arokiadaniel.a@goorulearning.org>
2014-10-23 18:09:08 +0530

    Async process debugging

--- rest-app/src/main/java/org/logger/event/web/service/EventServiceImpl.java
+++ rest-app/src/main/java/org/logger/event/web/service/EventServiceImpl.java
@@ -77,7 +77,7 @@ public class EventServiceImpl implements EventService {
 	private SimpleDateFormat minuteDateFormatter;
     
     public EventServiceImpl() {
-        dataLoaderService = new CassandraDataLoader();
+        //dataLoaderService = new CassandraDataLoader();
         this.connectionProvider = dataLoaderService.getConnectionProvider();
         baseDao = new BaseCassandraRepoImpl(connectionProvider);
         eventObjectValidator = new EventObjectValidator(null);
