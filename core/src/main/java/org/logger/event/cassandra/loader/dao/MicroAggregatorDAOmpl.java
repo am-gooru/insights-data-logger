324ef2ef7b9a42cd9d85e95dd33e1fa8878a30ce
daniel <arokiadaniel.a@goorulearning.org>
2014-10-28 19:24:36 +0530

    Debugging

--- core/src/main/java/org/logger/event/cassandra/loader/dao/MicroAggregatorDAOmpl.java
+++ core/src/main/java/org/logger/event/cassandra/loader/dao/MicroAggregatorDAOmpl.java
@@ -267,13 +267,13 @@ public class MicroAggregatorDAOmpl extends BaseDAOCassandraImpl implements Micro
 						
 						isStudent = baseCassandraDao.isUserPartOfClass(ColumnFamily.CLASSPAGE.getColumnFamily(),eventMap.get(GOORUID),classUid,0);
 							
-							int retryCount = 1;
+						/*	int retryCount = 1;
 					        while (!isStudent && retryCount < 2) {
 					        	Thread.sleep(1000);
 					        	isStudent = baseCassandraDao.isUserPartOfClass(ColumnFamily.CLASSPAGE.getColumnFamily(),eventMap.get(GOORUID),classUid,0);
 					        	logger.info("retrying to check if a student 2 : {}",retryCount);
 					            retryCount++;
-					        }
+					        }*/
 	
 						logger.info("isStudent : {}",isStudent);
 					
