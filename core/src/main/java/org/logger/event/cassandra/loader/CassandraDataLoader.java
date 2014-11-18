5f3fe6ff05fce34b79edd8477e410ddddcc17d8f
daniel <arokiadaniel.a@goorulearning.org>
2014-11-18 21:27:00 +0530

    Resource coulmnfamily migration

--- core/src/main/java/org/logger/event/cassandra/loader/CassandraDataLoader.java
+++ core/src/main/java/org/logger/event/cassandra/loader/CassandraDataLoader.java
@@ -1349,7 +1349,12 @@ public class CassandraDataLoader  implements Constants {
 									baseDao.generateNonCounter("resource",gooruOid,columnName+"B", searchResource.getColumnByIndex(x).getStringValue().equalsIgnoreCase("0") ? false:true, m);
 								}else if(columnName.equalsIgnoreCase("ratings.average") || columnName.equalsIgnoreCase("ratings.count") || columnName.equalsIgnoreCase("ratings.reviewCount")){
 									logger.info("columnName : " + columnName);
-									logger.info("columnValue : " + searchResource.getValue(columnName, (Serializer<Object>) new ObjectSerializer(),null));
+									try{
+										Object value = searchResource.getValue(columnName, (Serializer<Object>) new ObjectSerializer(),null)
+									logger.info("columnValue : " + value);
+									}catch(Exception ew){
+										ew.printStackTrace();
+									}
 									baseDao.generateNonCounter("resource",gooruOid,columnName, searchResource.getValue(columnName, (Serializer<Object>) new ObjectSerializer(), null), m);									
 								}
 								else{
