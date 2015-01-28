2b219bcdef8eabe49a47ea231380ba07d771fc02
daniel <arokiadaniel.a@goorulearning.org>
2015-01-28 19:20:50 +0530

    All session data migration

--- core/src/main/java/org/logger/event/cassandra/loader/CassandraDataLoader.java
+++ core/src/main/java/org/logger/event/cassandra/loader/CassandraDataLoader.java
@@ -815,7 +815,7 @@ public class CassandraDataLoader implements Constants {
     					
     						logger.info("isStudent : " + isStudent);
     						if(isStudent){
-    					/*		MutationBatch m1 = getConnectionProvider().getKeyspace().prepareMutationBatch().setConsistencyLevel(WRITE_CONSISTENCY_LEVEL);
+    							MutationBatch m1 = getConnectionProvider().getKeyspace().prepareMutationBatch().setConsistencyLevel(WRITE_CONSISTENCY_LEVEL);
     							MutationBatch m2 = getConnectionProvider().getKeyspace().prepareMutationBatch().setConsistencyLevel(WRITE_CONSISTENCY_LEVEL);
     							ColumnList<String> counterSessionData = baseDao.readWithKey(ColumnFamily.REALTIMECOUNTER.getColumnFamily(), (firstSessionId+"~"+keyPart),0);
     							for(int cs = 0 ; cs < counterSessionData.size() ; cs++ ){
@@ -841,7 +841,7 @@ public class CassandraDataLoader implements Constants {
     								
     							}
     							m1.execute();
-    							*/
+    							
     							MutationBatch m3 = getConnectionProvider().getKeyspace().prepareMutationBatch().setConsistencyLevel(WRITE_CONSISTENCY_LEVEL);
     							ColumnList<String> counterAllSessionData = baseDao.readWithKey(ColumnFamily.REALTIMECOUNTER.getColumnFamily(), ("AS~"+keyPartAll),0);
     							for(int as = 0 ; as < counterAllSessionData.size() ; as++ ){
