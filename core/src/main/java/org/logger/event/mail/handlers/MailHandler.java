cb709c601ba2de9d4fefe443c1a8bc1d355bfce9
kumaraswamy senapathy <kumaraswamy@goorulearning.org>
2015-03-12 18:32:05 +0530

    kafka:Changed kafka mail configuration in cache

--- core/src/main/java/org/logger/event/mail/handlers/MailHandler.java
+++ core/src/main/java/org/logger/event/mail/handlers/MailHandler.java
@@ -1,8 +1,9 @@
 package org.logger.event.mail.handlers;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
-import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -22,65 +23,72 @@ import com.netflix.astyanax.model.ColumnList;
 public class MailHandler implements Constants {
 
 	private static Session mailSession;
-	
+
 	private static BaseCassandraRepoImpl baseCassandraRepoImpl;
-	
+
+	private static Map<String, String> kafkaNotificationConfig;
+
 	private static Logger logger = LoggerFactory.getLogger(MailHandler.class);
 
-	public MailHandler(){
+	public MailHandler() {
 		baseCassandraRepoImpl = new BaseCassandraRepoImpl();
+		setKafkaConfiguration();
 	}
-	
-	private static void setProperty(final String userName, final String password){
+
+	private void setProperty(final String userName, final String password) {
 		Properties props = new Properties();
 		props.put("mail.transport.protocol", "smtp");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.port", "465");
 		mailSession = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-		protected PasswordAuthentication getPasswordAuthentication() {
-			return new PasswordAuthentication(userName,password);
-		}
-	});	
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, password);
+			}
+		});
 	}
-	
+
+	private void setKafkaConfiguration() {
+		kafkaNotificationConfig = new HashMap<String, String>();
+		ColumnList<String> columnList = baseCassandraRepoImpl.readWithKey(ColumnFamily.CONFIGSETTINGS.getColumnFamily(), KAFKA_MAIL_HANDLER, 0);
+		kafkaNotificationConfig.put("userName", columnList.getStringValue("user_name", null));
+		kafkaNotificationConfig.put("password", columnList.getStringValue("password", null));
+		kafkaNotificationConfig.put("toAdress", columnList.getStringValue("to_address", null));
+		kafkaNotificationConfig.put("ccAddress", columnList.getStringValue("cc_address", null));
+		kafkaNotificationConfig.put("bccAddress", columnList.getStringValue("bcc_address", null));
+		setProperty(columnList.getStringValue("user_name", null), columnList.getStringValue("password", null));
+	}
+
 	/**
 	 * This will send the kafka status notification using configuration
-	 * @param message The external message needs to be used in mail body.
+	 * 
+	 * @param message
+	 *            The external message needs to be used in mail body.
 	 */
-	public void sendKafkaNotification(String message){
-		try{
-		ColumnList<String> columnList = baseCassandraRepoImpl.readWithKey(ColumnFamily.CONFIGSETTINGS.getColumnFamily(), KAFKA_MAIL_HANDLER, 0);
-		String userName = columnList.getStringValue("user_name", null);
-		String password = columnList.getStringValue("password", null);
-		String toAddress = columnList.getStringValue("to_address", null);
-		String ccAddress = columnList.getStringValue("cc_address", null);
-		String bccAddress = columnList.getStringValue("bcc_address", null);
-		sendMail(userName, password, toAddress, ccAddress, bccAddress,"Kafka Notification",message);
-		}catch(Exception e){
+	public void sendKafkaNotification(String message) {
+		try {
+			sendMail(kafkaNotificationConfig.get("userName"), kafkaNotificationConfig.get("password"), kafkaNotificationConfig.get("toAdress"), kafkaNotificationConfig.get("ccAddress"),
+					kafkaNotificationConfig.get("bccAddress"), "Kafka Notification", message);
+		} catch (Exception e) {
 			logger.error("unable to send kafka notification");
 		}
 	}
-	
-	private static void sendMail(String userName,String password,String to,String cc,String bcc,String subject,String text) throws MessagingException {
-	
-		setProperty(userName, password);
+
+	private void sendMail(String userName, String password, String to, String cc, String bcc, String subject, String text) throws MessagingException {
+
 		Transport transport = mailSession.getTransport();
 		InternetAddress[] toRecipients = InternetAddress.parse(to);
 		MimeMessage message = new MimeMessage(mailSession);
-		message.setRecipients(Message.RecipientType.TO,
-				toRecipients);
-		if(cc != null){
+		message.setRecipients(Message.RecipientType.TO, toRecipients);
+		if (cc != null) {
 			InternetAddress[] ccRecipients = InternetAddress.parse(cc);
-			message.setRecipients(Message.RecipientType.CC,
-					ccRecipients);
+			message.setRecipients(Message.RecipientType.CC, ccRecipients);
 		}
-		if(bcc != null){
+		if (bcc != null) {
 			InternetAddress[] bccRecipients = InternetAddress.parse(bcc);
-			message.setRecipients(Message.RecipientType.BCC,
-					bccRecipients);
+			message.setRecipients(Message.RecipientType.BCC, bccRecipients);
 		}
 		message.setSubject(subject);
 		message.setText(text);
