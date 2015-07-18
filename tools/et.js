e25f8ce3cef53b83cb573deda8893885f87adf19
aagooru <amit@goorulearning.org>
2015-07-18 16:40:40 +0530

    Initial commit for et.js and its minified file. The minified file does not need a checkin and can be generated run-time by a specific magnification tool of choice.

---
+++ tools/et.js
@@ -0,0 +1,61 @@
+(function() {
+   window.onbeforeunload = function() {
+  };
+  if(typeof(_et.endPoint) == 'undefined' ||  _et.endPoint == '') {
+      _et.endPoint =  "logapi.goorulearning.org";
+  }
+     var apiEndpoint = ('https:' == document.location.protocol ? 'https://' : 'http://') + _et.endPoint +"/api/log";
+
+  var el = {
+    authenticate: function() {
+      var request = el.request();
+      request.open('POST',apiEndpoint + "/event/authenticate?apiKey=" + _et.apiKey , true);
+      request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+      request.onreadystatechange=function() {
+        if(request.readyState == 4) {
+          var responseType = request.getAllResponseHeaders();
+            if(responseType.indexOf("application/json") != -1) {
+                var json = eval("(" + request.responseText + ")");
+                if (request.status == 403) {
+                } else {
+                    self.setInterval(function(){el.triggerCall()}, 0);
+                }
+            }
+        }
+      }
+     request.send();
+    },
+
+    triggerCall: function() {
+      if (_et.data.length > 0) {
+          console.log(_et.data);
+            var request = el.request();
+            request.open('POST',apiEndpoint + "/event?apiKey=" + _et.apiKey ,true);
+            request.setRequestHeader("Content-type","application/json");
+            request.onreadystatechange=function() {
+                if(request.readyState == 4) {
+                var responseType = request.getAllResponseHeaders();
+                    if(responseType.indexOf("application/json") != -1) {
+                    var json = eval("(" + request.responseText + ")");
+                    }
+                }
+            }
+            request.send("[" + _et.data[0].toString() + "]");
+            _et.data.splice(0, 1);
+       }
+    },
+
+    request : function() {
+      var request;
+      if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        request=new XMLHttpRequest();
+      }
+      else {// code for IE6, IE5
+        request=new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      return request;
+    }
+  };
+  el.authenticate();
+})();
+
