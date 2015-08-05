62735750b60d9fb55f31740a351b7439381ca4ea
daniel <arokiadaniel.a@goorulearning.org>
2015-08-05 17:31:58 +0530

    updated et.js

--- tools/et.js
+++ tools/et.js
@@ -1,65 +1,36 @@
-(function() {
-  if(typeof(_et.endPoint) == 'undefined' ||  _et.endPoint == '') {
-      _et.endPoint =  "logapi.goorulearning.org";
-  }
- var apiEndpoint = ('https:' == document.location.protocol ? 'https://' : 'http://') + _et.endPoint +"/api/log";
-
-
-  var el = {
-    authenticate: function() {
-      var request = el.request();
-      request.open('POST',apiEndpoint + "/event/authenticate?apiKey=" + _et.apiKey , true);
-      request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-      request.onreadystatechange=function() {
-        if(request.readyState == 4) {
-          var responseType = request.getAllResponseHeaders();
-            if(responseType.indexOf("application/json") != -1) {
-                var json = eval("(" + request.responseText + ")");
-                if (request.status == 403) {
-                    if (window.console) {
-                        console.error("ET: Not able to authenticate");
+! function() {
+    window.onbeforeunload = function() {}, ("undefined" == typeof _et.endPoint || "" == _et.endPoint) && (_et.endPoint = "logapi.goorulearning.org");
+    var apiEndpoint = ("https:" == document.location.protocol ? "https://" : "http://") + _et.endPoint + "/api/log",
+        el = {
+            authenticate: function() {
+                var request = el.request();
+                request.open("POST", apiEndpoint + "/event/authenticate?apiKey=" + _et.apiKey, !0), request.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), request.onreadystatechange = function() {
+                    if (4 == request.readyState) {
+                        var responseType = request.getAllResponseHeaders();
+                        if (-1 != responseType.indexOf("application/json")) {
+                            var json = eval("(" + request.responseText + ")");
+                            403 == request.status || self.setInterval(function() {
+                                el.triggerCall()
+                            }, 0)
+                        }
                     }
-                } else {
-                    if (window.console) {
-                        console.debug("ET: Authentication successful");
-                    }
+                }, request.send()
+            },
+            triggerCall: function() {
+                if (_et.data.length > 0) {
+                    var request = el.request();
+                    request.open("POST", apiEndpoint + "/event?apiKey=" + _et.apiKey, !0), request.setRequestHeader("Content-type", "application/json"), request.onreadystatechange = function() {
+                        if (4 == request.readyState) {
+                            var responseType = request.getAllResponseHeaders();
+                            if (-1 != responseType.indexOf("application/json")) var json = eval("(" + request.responseText + ")")
+                        }
+                    }, request.send("[" + _et.data[0].toString() + "]"), _et.data.splice(0, 1)
                 }
+            },
+            request: function() {
+                var e;
+                return e = window.XMLHttpRequest ? new XMLHttpRequest : new ActiveXObject("Microsoft.XMLHTTP")
             }
-        }
-      }
-     request.send();
-    },
-
-    triggerCall: function() {
-      if (_et.data.length > 0) {
-            var request = el.request();
-            request.open('POST',apiEndpoint + "/event?apiKey=" + _et.apiKey, false);
-            request.setRequestHeader("Content-type","application/json");
-            request.onreadystatechange=function() {
-                if(request.readyState == 4) {
-                var responseType = request.getAllResponseHeaders();
-                    if(responseType.indexOf("application/json") != -1) {
-                    var json = eval("(" + request.responseText + ")");
-                    }
-                }
-            }
-            request.send("[" + _et.data[0].toString() + "]");
-            _et.data.splice(0, 1);
-       }
-    },
-
-    request : function() {
-      var request;
-      if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-        request=new XMLHttpRequest();
-      }
-      else {// code for IE6, IE5
-        request=new ActiveXObject("Microsoft.XMLHTTP");
-      }
-      return request;
-    }
-  };
-  el.authenticate();
- _et.triggerCall = el.triggerCall;
-})();
-
+        };
+    el.authenticate()
+}();
