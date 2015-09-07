720c5e824e1902abed04ad65869c780c51b89329
daniel <arokiadaniel.a@goorulearning.org>
2015-09-07 16:03:39 +0530

    Thread pool config change

--- rest-app/src/main/java/org/logger/event/web/controller/EventController.java
+++ rest-app/src/main/java/org/logger/event/web/controller/EventController.java
@@ -603,9 +603,9 @@ public class EventController implements Constants,AsyncConfigurer {
      @Override
 	public Executor getAsyncExecutor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(10);
-		executor.setMaxPoolSize(50);
-		executor.setQueueCapacity(100);
+		executor.setCorePoolSize(20);
+		executor.setMaxPoolSize(100);
+		executor.setQueueCapacity(200);
 		executor.setThreadNamePrefix("eventExecutor");
 		executor.initialize();
 		return executor;
