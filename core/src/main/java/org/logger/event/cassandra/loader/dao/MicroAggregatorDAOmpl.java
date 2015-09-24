8eeb40154859704b9bb88566de4f89510681274c
daniel <arokiadaniel.a@goorulearning.org>
2015-09-24 10:29:46 +0530

    Unit level key for usagedata store

--- core/src/main/java/org/logger/event/cassandra/loader/dao/MicroAggregatorDAOmpl.java
+++ core/src/main/java/org/logger/event/cassandra/loader/dao/MicroAggregatorDAOmpl.java
@@ -1609,7 +1609,7 @@ public class MicroAggregatorDAOmpl extends BaseDAOCassandraImpl implements Micro
 	private Set<String> generateUsageKeys(String classGooruId, String courseGooruId, String unitGooruId, String lessonGooruId,String contentGooruId) {
 		Set<String> usageKeys = new HashSet<String>();
 		usageKeys.add(generateColumnKey(classGooruId, courseGooruId, unitGooruId,lessonGooruId,contentGooruId));
-		usageKeys.add(generateColumnKey(classGooruId, courseGooruId, lessonGooruId));
+		usageKeys.add(generateColumnKey(classGooruId, courseGooruId, unitGooruId,lessonGooruId));
 		usageKeys.add(generateColumnKey(classGooruId, courseGooruId, unitGooruId));
 		usageKeys.add(generateColumnKey(classGooruId, courseGooruId));
 		usageKeys.add(classGooruId);
