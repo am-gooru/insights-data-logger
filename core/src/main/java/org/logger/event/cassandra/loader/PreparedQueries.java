aa45fc78d8cabbca44dad41bcae9e1c6ac1b2292
Daniel <arokiadaniel.a@bambeeq.com>
2016-03-16 17:09:37 +0530

    parepared statement queries

--- core/src/main/java/org/logger/event/cassandra/loader/PreparedQueries.java
+++ core/src/main/java/org/logger/event/cassandra/loader/PreparedQueries.java
@@ -14,160 +14,115 @@ public final class PreparedQueries extends BaseDAOCassandraImpl {
 		return PreparedQueriesHolder.INSTANCE;
 	}
 
-	private PreparedStatement INSERT_USER_SESSION = null;
+	private final PreparedStatement INSERT_USER_SESSION = getCassSession().prepare(Constants.INSERT_USER_SESSION);
 
-	private PreparedStatement INSERT_USER_LAST_SESSION = null;
+	private final PreparedStatement INSERT_USER_LAST_SESSION = getCassSession().prepare(Constants.INSERT_USER_LAST_SESSION);
 
-	private PreparedStatement INSERT_USER_SESSION_ACTIVITY = null;
+	private final PreparedStatement INSERT_USER_SESSION_ACTIVITY = getCassSession().prepare(Constants.INSERT_USER_SESSION_ACTIVITY);
 
-	private PreparedStatement INSERT_STUDENTS_CLASS_ACTIVITY = null;
+	private final PreparedStatement INSERT_STUDENTS_CLASS_ACTIVITY = getCassSession().prepare(Constants.INSERT_STUDENTS_CLASS_ACTIVITY);
 
-	private PreparedStatement INSERT_CONTENT_TAXONOMY_ACTIVITY = null;
+	private final PreparedStatement INSERT_CONTENT_TAXONOMY_ACTIVITY = getCassSession().prepare(Constants.INSERT_CONTENT_TAXONOMY_ACTIVITY);
 
 	private final  PreparedStatement INSERT_CONTENT_CLASS_TAXONOMY_ACTIVITY = getCassSession().prepare(Constants.INSERT_CONTENT_CLASS_TAXONOMY_ACTIVITY);
 
-	private PreparedStatement INSERT_USER_LOCATION = null;
+	private final PreparedStatement INSERT_USER_LOCATION = getCassSession().prepare(Constants.INSERT_USER_LOCATION);
 
-	private PreparedStatement UPDATE_PEER_COUNT = null;
+	private final PreparedStatement UPDATE_PEER_COUNT = getCassSession().prepare(Constants.UPDATE_PEER_COUNT);
 
-	// private PreparedStatement UPDATE_PEER_DETAILS_ON_START = getCassSession().prepare(Constants.UPDATE_PEER_DETAILS_ON_START);
+	// private final PreparedStatement UPDATE_PEER_DETAILS_ON_START = getCassSession().prepare(Constants.UPDATE_PEER_DETAILS_ON_START);
 
-	// private PreparedStatement UPDATE_PEER_DETAILS_ON_STOP = getCassSession().prepare(Constants.UPDATE_PEER_DETAILS_ON_STOP);
+	// private final PreparedStatement UPDATE_PEER_DETAILS_ON_STOP = getCassSession().prepare(Constants.UPDATE_PEER_DETAILS_ON_STOP);
 
-	private PreparedStatement SELECT_USER_SESSION_ACTIVITY = null;
+	private final PreparedStatement SELECT_USER_SESSION_ACTIVITY = getCassSession().prepare(Constants.SELECT_USER_SESSION_ACTIVITY);
 
-	private PreparedStatement SELECT_USER_SESSION_ACTIVITY_BY_SESSION_ID = null;
+	private final PreparedStatement SELECT_USER_SESSION_ACTIVITY_BY_SESSION_ID = getCassSession().prepare(Constants.SELECT_USER_SESSION_ACTIVITY_BY_SESSION_ID);
 
-	private PreparedStatement SELECT_STUDENTS_CLASS_ACTIVITY = null;
+	private final PreparedStatement SELECT_STUDENTS_CLASS_ACTIVITY = getCassSession().prepare(Constants.SELECT_STUDENTS_CLASS_ACTIVITY);
 
-	private PreparedStatement UPDATE_REACTION = null;
+	private final PreparedStatement UPDATE_REACTION = getCassSession().prepare(Constants.UPDATE_REACTION);
 
-	private PreparedStatement UPDATE_SESSION_SCORE = null;
+	private final PreparedStatement UPDATE_SESSION_SCORE = getCassSession().prepare(Constants.UPDATE_SESSION_SCORE);
 
-	private PreparedStatement SELECT_CLASS_ACTIVITY_DATACUBE = null;
+	private final PreparedStatement SELECT_CLASS_ACTIVITY_DATACUBE = getCassSession().prepare(Constants.SELECT_CLASS_ACTIVITY_DATACUBE);
 
-	private PreparedStatement SELECT_ALL_CLASS_ACTIVITY_DATACUBE = null;
+	private final PreparedStatement SELECT_ALL_CLASS_ACTIVITY_DATACUBE = getCassSession().prepare(Constants.SELECT_ALL_CLASS_ACTIVITY_DATACUBE);
 
 	private final PreparedStatement INSERT_CLASS_ACTIVITY_DATACUBE = getCassSession().prepare(Constants.INSERT_CLASS_ACTIVITY_DATACUBE);
 
-	private PreparedStatement SELECT_TAXONOMY_PARENT_NODE = null;
+	private final PreparedStatement SELECT_TAXONOMY_PARENT_NODE = getCassSession().prepare(Constants.SELECT_TAXONOMY_PARENT_NODE);
 
-	private PreparedStatement SELECT_CONTENT_TAXONOMY_ACTIVITY = null;
+	private final PreparedStatement SELECT_CONTENT_TAXONOMY_ACTIVITY = getCassSession().prepare(Constants.SELECT_CONTENT_TAXONOMY_ACTIVITY);
 
-	private PreparedStatement SELECT_CONTENT_CLASS_TAXONOMY_ACTIVITY = null;
+	private final PreparedStatement SELECT_CONTENT_CLASS_TAXONOMY_ACTIVITY = getCassSession().prepare(Constants.SELECT_CONTENT_CLASS_TAXONOMY_ACTIVITY);
 
-	private PreparedStatement SELECT_TAXONOMY_ACTIVITY_DATACUBE = null;
+	private final PreparedStatement SELECT_TAXONOMY_ACTIVITY_DATACUBE = getCassSession().prepare(Constants.SELECT_TAXONOMY_ACTIVITY_DATACUBE);
 
-	private PreparedStatement INSERT_USER_QUESTION_GRADE = null;
+	private final PreparedStatement INSERT_USER_QUESTION_GRADE = getCassSession().prepare(Constants.INSERT_USER_QUESTION_GRADE);
 
-	private PreparedStatement SELECT_USER_QUESTION_GRADE_BY_SESSION = null;
+	private final PreparedStatement SELECT_USER_QUESTION_GRADE_BY_SESSION = getCassSession().prepare(Constants.SELECT_USER_QUESTION_GRADE_BY_SESSION);
 
-	private PreparedStatement SELECT_USER_QUESTION_GRADE_BY_QUESTION = null;
+	private final PreparedStatement SELECT_USER_QUESTION_GRADE_BY_QUESTION = getCassSession().prepare(Constants.SELECT_USER_QUESTION_GRADE_BY_QUESTION);
 
-	private PreparedStatement INSERT_TAXONOMY_ACTIVITY_DATACUBE = null;
+	private final PreparedStatement INSERT_TAXONOMY_ACTIVITY_DATACUBE = getCassSession().prepare(Constants.INSERT_TAXONOMY_ACTIVITY_DATACUBE);
 
 	public PreparedStatement updateSessionScore() {
-		if (UPDATE_SESSION_SCORE == null) {
-			UPDATE_SESSION_SCORE = getCassSession().prepare(Constants.UPDATE_SESSION_SCORE);
-		}
 		return UPDATE_SESSION_SCORE;
 	}
 
 	public PreparedStatement insertUserSession() {
-		if (INSERT_USER_SESSION == null) {
-			INSERT_USER_SESSION = getCassSession().prepare(Constants.INSERT_USER_SESSION);
-		}
 		return INSERT_USER_SESSION;
 	}
 
 	public PreparedStatement insertUserLastSession() {
-		if (INSERT_USER_LAST_SESSION == null) {
-			INSERT_USER_LAST_SESSION = getCassSession().prepare(Constants.INSERT_USER_LAST_SESSION);
-		}
 		return INSERT_USER_LAST_SESSION;
 	}
 
 	public PreparedStatement insertUserSessionActivity() {
-		if (INSERT_USER_SESSION_ACTIVITY == null) {
-			INSERT_USER_SESSION_ACTIVITY = getCassSession().prepare(Constants.INSERT_USER_SESSION_ACTIVITY);
-		}
 		return INSERT_USER_SESSION_ACTIVITY;
 	}
 
 	public PreparedStatement insertStudentsClassActivity() {
-		if (INSERT_STUDENTS_CLASS_ACTIVITY == null) {
-			INSERT_STUDENTS_CLASS_ACTIVITY = getCassSession().prepare(Constants.INSERT_STUDENTS_CLASS_ACTIVITY);
-		}
 		return INSERT_STUDENTS_CLASS_ACTIVITY;
 	}
 
 	public PreparedStatement insertContentTaxonomyActivity() {
-		if (INSERT_CONTENT_TAXONOMY_ACTIVITY == null) {
-			INSERT_CONTENT_TAXONOMY_ACTIVITY = getCassSession().prepare(Constants.INSERT_CONTENT_TAXONOMY_ACTIVITY);
-		}
 		return INSERT_CONTENT_TAXONOMY_ACTIVITY;
 	}
 
 	public PreparedStatement insertContentClassTaxonomyActivty() {
-		//if (INSERT_CONTENT_CLASS_TAXONOMY_ACTIVITY == null) {
-			//INSERT_CONTENT_CLASS_TAXONOMY_ACTIVITY = getCassSession().prepare(Constants.INSERT_CONTENT_CLASS_TAXONOMY_ACTIVITY);
-		//}
 		return INSERT_CONTENT_CLASS_TAXONOMY_ACTIVITY;
 	}
 
 	public PreparedStatement insertUserLastLocation() {
-		if (INSERT_USER_LOCATION == null) {
-			INSERT_USER_LOCATION = getCassSession().prepare(Constants.INSERT_USER_LOCATION);
-		}
 		return INSERT_USER_LOCATION;
 	}
 
 	public PreparedStatement updatePeerCount() {
-		if (UPDATE_PEER_COUNT == null) {
-			UPDATE_PEER_COUNT = getCassSession().prepare(Constants.UPDATE_PEER_COUNT);
-		}
 		return UPDATE_PEER_COUNT;
 	}
 
 	public PreparedStatement selectUserSessionActivity() {
-		if (SELECT_USER_SESSION_ACTIVITY == null) {
-			SELECT_USER_SESSION_ACTIVITY = getCassSession().prepare(Constants.SELECT_USER_SESSION_ACTIVITY);
-		}
 		return SELECT_USER_SESSION_ACTIVITY;
 	}
 
 	public PreparedStatement selectUserSessionActivityBySessionId() {
-		if (SELECT_USER_SESSION_ACTIVITY_BY_SESSION_ID == null) {
-			return getCassSession().prepare(Constants.SELECT_USER_SESSION_ACTIVITY_BY_SESSION_ID);
-		}
 		return SELECT_USER_SESSION_ACTIVITY_BY_SESSION_ID;
 	}
 
 	public PreparedStatement selectStudentClassActivity() {
-		if (SELECT_STUDENTS_CLASS_ACTIVITY == null) {
-			return getCassSession().prepare(Constants.SELECT_STUDENTS_CLASS_ACTIVITY);
-		}
 		return SELECT_STUDENTS_CLASS_ACTIVITY;
 	}
 
 	public PreparedStatement updateReaction() {
-		if (UPDATE_REACTION == null) {
-			UPDATE_REACTION = getCassSession().prepare(Constants.UPDATE_REACTION);
-		}
 		return UPDATE_REACTION;
 	}
 
 	public PreparedStatement selectClassActivityDataCube(PreparedStatement statement) {
-		if (SELECT_CLASS_ACTIVITY_DATACUBE == null) {
-			SELECT_CLASS_ACTIVITY_DATACUBE = getCassSession().prepare(Constants.SELECT_CLASS_ACTIVITY_DATACUBE);
-		}
 		return SELECT_CLASS_ACTIVITY_DATACUBE;
 	}
 
 	public PreparedStatement selectAllClassActivityDataCube() {
-		if (SELECT_ALL_CLASS_ACTIVITY_DATACUBE == null) {
-			SELECT_ALL_CLASS_ACTIVITY_DATACUBE = getCassSession().prepare(Constants.SELECT_ALL_CLASS_ACTIVITY_DATACUBE);
-		}
 		return SELECT_ALL_CLASS_ACTIVITY_DATACUBE;
 	}
 
@@ -176,58 +131,34 @@ public final class PreparedQueries extends BaseDAOCassandraImpl {
 	}
 
 	public PreparedStatement selectTaxonomyParentNode() {
-		if (SELECT_TAXONOMY_PARENT_NODE == null) {
-			SELECT_TAXONOMY_PARENT_NODE = getCassSession().prepare(Constants.SELECT_TAXONOMY_PARENT_NODE);
-		}
 		return SELECT_TAXONOMY_PARENT_NODE;
 	}
 
 	public PreparedStatement selectContentTaxonomyActivity() {
-		if (SELECT_CONTENT_TAXONOMY_ACTIVITY == null) {
-			SELECT_CONTENT_TAXONOMY_ACTIVITY = getCassSession().prepare(Constants.SELECT_CONTENT_TAXONOMY_ACTIVITY);
-		}
 		return SELECT_CONTENT_TAXONOMY_ACTIVITY;
 	}
 
 	public PreparedStatement selectContentClassTaxonomyActivity() {
-		if (SELECT_CONTENT_CLASS_TAXONOMY_ACTIVITY == null) {
-			return getCassSession().prepare(Constants.SELECT_CONTENT_CLASS_TAXONOMY_ACTIVITY);
-		}
 		return SELECT_CONTENT_CLASS_TAXONOMY_ACTIVITY;
 	}
 
 	public PreparedStatement selectTaxonomyActivityDataCube() {
-		if (SELECT_TAXONOMY_ACTIVITY_DATACUBE == null) {
-			SELECT_TAXONOMY_ACTIVITY_DATACUBE = getCassSession().prepare(Constants.SELECT_TAXONOMY_ACTIVITY_DATACUBE);
-		}
 		return SELECT_TAXONOMY_ACTIVITY_DATACUBE;
 	}
 
 	public PreparedStatement insertUserQuestionGrade() {
-		if (INSERT_USER_QUESTION_GRADE == null) {
-			INSERT_USER_QUESTION_GRADE = getCassSession().prepare(Constants.INSERT_USER_QUESTION_GRADE);
-		}
 		return INSERT_USER_QUESTION_GRADE;
 	}
 
 	public PreparedStatement selectUserQuestionGradeBySession() {
-		if (SELECT_USER_QUESTION_GRADE_BY_SESSION == null) {
-			SELECT_USER_QUESTION_GRADE_BY_SESSION = getCassSession().prepare(Constants.SELECT_USER_QUESTION_GRADE_BY_SESSION);
-		}
 		return SELECT_USER_QUESTION_GRADE_BY_SESSION;
 	}
 
 	public PreparedStatement selectUserQuestionGradeByQuestion() {
-		if (SELECT_USER_QUESTION_GRADE_BY_QUESTION == null) {
-			SELECT_USER_QUESTION_GRADE_BY_QUESTION = getCassSession().prepare(Constants.SELECT_USER_QUESTION_GRADE_BY_QUESTION);
-		}
 		return SELECT_USER_QUESTION_GRADE_BY_QUESTION;
 	}
 
 	public PreparedStatement insertTaxonomyActivityDataCube() {
-		if (INSERT_TAXONOMY_ACTIVITY_DATACUBE == null) {
-			INSERT_TAXONOMY_ACTIVITY_DATACUBE = getCassSession().prepare(Constants.INSERT_TAXONOMY_ACTIVITY_DATACUBE);
-		}
 		return INSERT_TAXONOMY_ACTIVITY_DATACUBE;
 	}
 }
