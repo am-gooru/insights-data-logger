0abed156aaba1966370e86111e7e83d94de36a2c
Daniel <arokiadaniel.a@bambeeq.com>
2016-04-15 13:53:40 +0530

    invalid columnfamily name

--- core/src/main/java/org/logger/event/cassandra/loader/Constants.java
+++ core/src/main/java/org/logger/event/cassandra/loader/Constants.java
@@ -881,7 +881,7 @@ public class Constants {
 
  	public static final String UPDATE_USER_STATISTICAL_COUNTER_DATA = "UPDATE statistical_user_data SET metrics_value = metrics_value+? WHERE clustering_key = ? AND user_uid = ? AND metrics_name = ?";
  	
- 	public static final String SELECT_USER_STATISTICAL_COUNTER_DATA = "SELECT metrics_value AS metrics FROM statistical_data WHERE clustering_key = ? AND user_uid = ? AND metrics_name = ?";
+ 	public static final String SELECT_USER_STATISTICAL_COUNTER_DATA = "SELECT metrics_value AS metrics FROM statistical_user_data WHERE clustering_key = ? AND user_uid = ? AND metrics_name = ?";
  	
  	public static final String SELECT_API_KEY = "SELECT * FROM app_api_key WHERE key = ?";
  	
