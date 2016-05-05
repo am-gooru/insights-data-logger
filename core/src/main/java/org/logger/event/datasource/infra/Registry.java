954ea85b26577d5067ca2af6834ac3b9cea4ad12
Daniel <arokiadaniel.a@bambeeq.com>
2016-05-05 17:53:20 +0530

    ELS connection disable

--- core/src/main/java/org/logger/event/datasource/infra/Registry.java
+++ core/src/main/java/org/logger/event/datasource/infra/Registry.java
@@ -41,7 +41,10 @@ public class Registry implements Iterable<Register> {
 	public Registry() {
 		registers = new ArrayList<Register>();
 		registers.add(CassandraClient.instance());
+		/**
+		 * To be Enable 
 		registers.add(ELSClient.instance());
+		*/
 		internalIterator = registers.iterator();
 	}
 
